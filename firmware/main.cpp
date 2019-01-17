ffd98cee93c8fd8e36a0805cbb825777f0f6344b
Nathan Robinson <nrobinson2000@me.com>
2019-01-17 16:38:38 -0500

    at 16:38 of 2019-01-17

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -3,6 +3,13 @@
 SYSTEM_MODE(SEMI_AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
 
+// Cloud Variables Global
+
+char ssid[100];
+int rssi;
+int strength;
+int quality;
+
 void startupTasks() {
         WiFi.selectAntenna(ANT_AUTO);
         RGB.control(true);
@@ -26,25 +33,27 @@ void listAPs()
         Serial.println();
 }
 
-void connectionStats()
+void updateConnectionStats()
 {
-  WiFiSignal sig = WiFi.RSSI();
-  int rssi = sig.rssi;
-  float strength = sig.getStrength();
-  float quality = sig.getQuality();
+        WiFiSignal sig = WiFi.RSSI();
 
-  Serial.printlnf("[%s]: rssi: %d, strength: %1.2f, quality: %1.2f", WiFi.SSID(), rssi, strength, quality);
+        snprintf(ssid, sizeof(ssid), "%s", WiFi.SSID());
+        rssi = sig.rssi;
+        strength = sig.getStrength();
+        quality = sig.getQuality();
+
+        Serial.printlnf("[%s]: rssi: %d, strength: %d, quality: %d", ssid, rssi, strength, quality);
 }
 
 void testByteToString()
 {
-  const uint8_t myBytes[] = {0xff, 0x0d, 0x03};
-  char myBytesBuffer[255];
-  for (unsigned i=0; i < sizeof(myBytes); i++) {
-    snprintf(myBytesBuffer, sizeof(myBytesBuffer), "%s%02X", myBytesBuffer, myBytes[i]);
-  }
+        const uint8_t myBytes[] = {0xff, 0x0d, 0x03};
+        char myBytesBuffer[255];
+        for (unsigned i=0; i < sizeof(myBytes); i++) {
+                snprintf(myBytesBuffer, sizeof(myBytesBuffer), "%s%02X", myBytesBuffer, myBytes[i]);
+        }
 
-  Serial.println(myBytesBuffer);
+        Serial.println(myBytesBuffer);
 }
 
 int toggleLed(const char* args) {
@@ -58,14 +67,39 @@ int toggleLed(const char* args) {
         return command;
 }
 
+const uint32_t tickDebounce = 200 * 1000 * SYSTEM_US_TICKS; // 50ms debounce period
+volatile bool ledState;
+
+void blink() {
+        static uint32_t lastTick = 0;
+        if (SYSTEM_TICK_COUNTER - lastTick < tickDebounce) return;
+        lastTick = SYSTEM_TICK_COUNTER;
+        ledState = !ledState;
+
+        digitalWrite(D0, ledState);
+}
 
 void setup() // Put setup code here to run once
 {
+        // Button and LED
+        pinMode(D0, OUTPUT);
+        pinMode(A0, INPUT_PULLUP);
+        attachInterrupt(A0, blink, FALLING);
+
+        // Initialize Objects
         Serial.begin(115200);
         WiFi.on();
 
+        // Cloud Variable Definitions
+        Particle.variable("ssid", ssid);
+        Particle.variable("rssi", rssi);
+        Particle.variable("strength", strength);
+        Particle.variable("quality", quality);
+
+        // Cloud Function Definitions
         Particle.function("toggle", toggleLed);
 
+        // Attempt Particle Connection
         Particle.connect();
 }
 
@@ -82,10 +116,10 @@ void loop() // Put code here to loop forever
         if (millis() - msDelay >= 3000) {
 
                 if (WiFi.ready()) {
-                  connectionStats();
-                  //testByteToString();
+                        updateConnectionStats();
+                        //testByteToString();
                 } else {
-                    listAPs();
+                        listAPs();
                 }
 
                 msDelay = millis();
